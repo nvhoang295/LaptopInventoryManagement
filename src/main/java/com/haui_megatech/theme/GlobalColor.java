d5ff1f3adbe28beae70089b3b31e5b3eec7c4237
Nguyen Viet Hoang <hoangnv.swe@gmail.com>
2024-05-03 02:53:08 +0700

    Add home layout

---
+++ src/main/java/com/haui_megatech/theme/GlobalColor.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.haui_megatech.theme;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author vieth
+ */
+public class GlobalColor {
+
+    public static final Color WHITE = new Color(255, 255, 255);
+    public static final Color BLACK = new Color(0, 0, 0);
+    public static final Color BLACK_HOVER = new Color(32, 32, 32);
+    public static final Color DARK = new Color(33, 37, 41);
+    public static final Color DARK_HOVER = new Color(66, 70, 73);
+    public static final Color PRIMARY = new Color(13, 110, 253);
+    public static final Color DARK_PRIMARY = new Color(0, 86, 215);
+    public static final Color DANGER = new Color(220, 53, 69);
+    public static final Color DARK_DANGER = new Color(205, 51, 66);
+    public static final Color SUCCESS = new Color(22, 171, 102);
+    public static final Color DARK_SUCCESS = new Color(16, 133, 79);
+    public static final Color WARNING = new Color(255, 193, 7);
+    public static final Color DARK_WARNING = new Color(219, 167, 10);
+    public static final Color LIGHT_WARNING = new Color(255, 203, 46);
+    public static final Color TRANSPARENT = new Color(0, 0, 0, 0);
+    public static final Color WHITE_HOVER = new Color(230, 230, 230);
+    public static final Color SECONDARY = new Color(108, 117, 125);
+    public static final Color DARK_SECONDARY = new Color(86, 94, 100);
+    
+}
