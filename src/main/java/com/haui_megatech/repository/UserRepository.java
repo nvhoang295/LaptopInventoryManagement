39b76765f284bc7087ac61fe18657e277b262c37
Nguyen Viet Hoang <hoangnv.swe@gmail.com>
2024-05-05 05:21:27 +0700

    Add UserRepository and implement

---
+++ src/main/java/com/haui_megatech/repository/UserRepository.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.haui_megatech.repository;
+
+import com.haui_megatech.model.User;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ *
+ * @author vieth
+ */
+public interface UserRepository {
+
+    Optional<User> save(User user);
+
+    List<User> saveAll(List<User> users);
+
+    Optional<User> findByUsername(String username);
+
+    List<User> getList();
+}
---
+++ src/main/java/com/haui_megatech/repository/impl/UserRepositoryImpl.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.haui_megatech.repository.impl;
+
+import com.haui_megatech.model.User;
+import com.haui_megatech.repository.UserRepository;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ *
+ * @author vieth
+ */
+public class UserRepositoryImpl implements UserRepository {
+
+    @Override
+    public Optional<User> save(User user) {
+        List<User> users;
+        try (ObjectInputStream ois = new ObjectInputStream((new FileInputStream("")))) {
+            users = (List<User>) ois.readObject();
+            if (users == null) {
+                users = new ArrayList<>();
+            }
+            users.add(user);
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(""))) {
+            oos.writeObject(users);
+            return Optional.of(user);
+        } catch (IOException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public List<User> saveAll(List<User> users) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(""))) {
+            oos.writeObject(users);
+            return users;
+        } catch (IOException e) {
+            return List.of();
+        }
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        List<User> users = this.getList();
+        return users.parallelStream().filter(item -> item.getUsername().equals(username)).findFirst();
+    }
+
+    @Override
+    public List<User> getList() {
+        List<User> users;
+        try (ObjectInputStream ois = new ObjectInputStream((new FileInputStream("")))) {
+            users = (List<User>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return users;
+    }
+
+}
