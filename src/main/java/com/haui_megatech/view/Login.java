149575812bb403438c2ef3e9bdbe47363489549d
Nguyen Viet Hoang <hoangnv.swe@gmail.com>
2024-05-05 13:09:26 +0700

    Rearrange reset password label

--- src/main/java/com/haui_megatech/view/Login.java
+++ src/main/java/com/haui_megatech/view/Login.java
@@ -35,7 +35,7 @@ public class Login extends javax.swing.JFrame {
         sendRestEmailButton = new javax.swing.JButton();
         sendResetEmailLabel = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         sendResetCodePanel = new javax.swing.JPanel();
@@ -63,7 +63,6 @@ public class Login extends javax.swing.JFrame {
         recoveryPasswordDiaglog.setTitle("Khôi phục mật khẩu");
         recoveryPasswordDiaglog.setBackground(new java.awt.Color(255, 255, 255));
         recoveryPasswordDiaglog.setMinimumSize(new java.awt.Dimension(700, 500));
-        recoveryPasswordDiaglog.setPreferredSize(new java.awt.Dimension(700, 500));
         recoveryPasswordDiaglog.setSize(new java.awt.Dimension(700, 500));
         recoveryPasswordDiaglog.getContentPane().setLayout(null);
 
@@ -90,28 +89,27 @@ public class Login extends javax.swing.JFrame {
 
         jPanel1.setBackground(new java.awt.Color(44, 43, 196));
 
-        jLabel1.setBackground(new java.awt.Color(44, 43, 196));
-        jLabel1.setOpaque(true);
-        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("KHÔI PHỤC MẬT KHẨU");
+        jLabel5.setBackground(new java.awt.Color(44, 43, 196));
+        jLabel5.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel5.setText("KHÔI PHỤC MẬT KHẨU");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 688, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, 688, Short.MAX_VALUE)
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, 108, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         sendResetEmailPanel.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 700, 120));
@@ -396,10 +394,10 @@ public class Login extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel dangNhapTittle;
     private javax.swing.JLabel forget;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
